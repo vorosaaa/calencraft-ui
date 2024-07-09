b3b1c6820220100f070746c0055dd5dbf605e54c
Vörös Attila <vorosattila@icloud.com>
2024-07-09 22:41:53 +0200

    Try to fix github build

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,4 +2,5 @@ export const config = {
   NAME: "Calencraft",
   SERVER_URL: process.env.REACT_APP_SERVER_URL || "http://localhost:8080",
   MODE: process.env.REACT_APP_NODE_ENV || "development",
+  STRIPE_PUBLIC_KEY: process.env.REACT_APP_STRIPE_PUBLIC_KEY || "public",
 };
